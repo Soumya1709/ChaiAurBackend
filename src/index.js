7990ef7d75ac0d6d5fe32e394d7b8b4583983837
Soumya1709 <soumyaranchi05@gmail.com>
2025-10-08 20:57:37 +0530

    utils

---
+++ src/index.js
@@ -0,0 +1,38 @@
+//require("dotenv").config({path: './env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`process.env.PORT : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("MONOGODB connection failed !!!",err);
+})
+
+
+/*
+import express from "express";
+const app = express();
+( async () => {
+    try{
+        await  mongoose.connect(`${Process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR:",error);
+            throw error;
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server started on PORT ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log("ERROR:", error);
+        throw error;
+    }
+})()*/
